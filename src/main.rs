bfbab3b9c8d61f2e488e816ef474e59dcdc675e9
Ire Alarape <ialarape@andrew.cmu.edu>
2023-04-02 16:28:48 -0400

    Trie Boiler Plate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+// Boiler plate src: https://iq.opengenus.org/trie-in-rust/
+
+pub struct TrieNode {
+    value: char,
+    is_final: bool,
+    child_nodes: Vec<Box<TrieNode>>,
+}
+
+impl TrieNode {
+    // Create new node
+    pub fn create(c: char, is_final: bool) -> TrieNode {}
+    // Check if a node has that value
+    pub fn check_value(self, c: char) -> bool {
+        return true;
+    }
+}
+
+struct TrieStruct {
+    root_node: TrieNode,
+}
+
+impl TrieStruct {
+    // Insert a string
+    pub fn insert(string_val: String) {}
+    // Find a string
+    pub fn find(string_val: String) -> bool {
+        return true;
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // Create Trie
+    // Insert Stuff
+    // Find Stuff
+    // Delete
+
+    // Stretch Goal: 
+    // Allow concurrency...
+    // Maybe order tree in some way
+
 }
